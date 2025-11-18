1553c40f6b607e86542e90c16df357ec63837ff5
Acbox <acbox0328@gmail.com>
2025-11-18 21:54:35 +0800

    feat: slider & variables expose

---
+++ packages/shared/src/form.ts
@@ -0,0 +1,15 @@
+export enum FormType {
+  SLIDER = 'slider',
+}
+
+export type BaseForm<T extends FormType> = {
+  type: T
+  bind: string
+}
+
+export type SliderForm = BaseForm<FormType.SLIDER> & {
+  min: number
+  max: number
+  step: number
+  value: number
+}
